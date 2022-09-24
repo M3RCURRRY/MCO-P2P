2ee6eee721ee701c38dd35a30cd608f84cf11d38
M3RCURRRY <m3rcurrry1337@gmail.com>
2022-09-25 00:14:00 +0300

    UI fix

--- mco-p2p/src/routes/IndexRoute/IndexRoute.tsx
+++ mco-p2p/src/routes/IndexRoute/IndexRoute.tsx
@@ -11,20 +11,6 @@ import "./IndexRoute.scss";
 import RouteCard from "../../components/RouteCard/RouteCard";
 import { Container, Row, Col } from "react-bootstrap";
 
-const firstRouteRow: React.ReactNode = (
-  <>
-    <RouteCard title="Market" src={marketRouteImg} alt="Market Route" />
-    <RouteCard title="Trades" src={tradeRouteImg} alt="Trade Route" />
-    <RouteCard title="Server Map" src={mapRouteImg} alt="Map Route" />
-  </>
-);
-
-const secondRouteRow: React.ReactNode = (
-  <>
-    <RouteCard title="Calculator" src={calcRouteImg} alt="Calc Route" />
-  </>
-)
-
 export default function IndexRoute() {
   const [isVisible, setVisible] = useState(false);
 
@@ -35,20 +21,58 @@ export default function IndexRoute() {
   }, []);
 
   return (
-    <div style={{marginTop: "35px"}}>
-      <Container>
-        <Row>
-          <Col sm={2} md={3}></Col>
-          <Col xs={12} sm={8} md={6}>
-            <Transition in={isVisible} timeout={500} mountOnEnter unmountOnExit>
-              {(state) => (
-                <div className={`routeList ${state}`}>{firstRouteRow}</div>
-              )}
-            </Transition>
-          </Col>
-          <Col sm={2} md={3}></Col>
-        </Row>
-      </Container>
+    <div style={{ marginTop: "35px" }}>
+      <Transition in={isVisible} timeout={500} mountOnEnter unmountOnExit>
+        {(state) => (
+          <Container className={`routeList ${state}`}>
+            <Row style={{marginBottom: "25px"}}>
+              <Col lg={3}></Col>
+              <Col xs={4} sm={4} md={4} lg={2} className="alignRoute">
+                <RouteCard
+                  title="Market"
+                  src={marketRouteImg}
+                  alt="Market Route"
+                />
+              </Col>
+              <Col xs={4} sm={4} md={4} lg={2} className="alignRoute">
+                <RouteCard
+                  title="Trades"
+                  src={tradeRouteImg}
+                  alt="Trade Route"
+                />
+              </Col>
+              <Col xs={4} sm={4} md={4} lg={2} className="alignRoute">
+                <RouteCard
+                  title="Server Map"
+                  src={mapRouteImg}
+                  alt="Map Route"
+                />
+              </Col>
+              <Col lg={3}></Col>
+            </Row>
+
+            <Row>
+              <Col lg={3}></Col>
+              <Col xs={4} sm={4} md={4} lg={2}></Col>
+              <Col xs={4} sm={4} md={4} lg={2} className="alignRoute">
+                <RouteCard
+                  title="Calculator"
+                  src={calcRouteImg}
+                  alt="Calc Route"
+                />
+              </Col>
+              <Col xs={4} sm={4} md={4} lg={2}></Col>
+              <Col lg={3}></Col>
+            </Row>
+          </Container>
+        )}
+      </Transition>
     </div>
   );
 }
+
+/*
+
+<RouteCard title="Calculator" src={calcRouteImg} alt="Calc Route" />
+
+*/
